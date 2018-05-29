6e715ebbfaa4c12f4cd1d8d29b4dd06a3b3b4971
mayragit <mbanuelo@mail.sfsu.edu>
2018-05-29 14:30:23 -0700

    testing

---
+++ practice.py
@@ -0,0 +1,12 @@
+fileObj = open('Downloads/PythonForBiology/examples/chromoData.tsv')
+values = []
+header = fileObj.readline()
+
+for line in fileObj:
+   data = line.split()
+   chromosome, position, value = data
+   position = int(position)
+   value = float(value)
+
+   values.append(value)
+   print(sum(values)/len(values))
